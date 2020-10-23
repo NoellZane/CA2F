24aa78740dd1aa5f22afcd75388cbe619ee88206
Noell <cph-ng67@cphbusiness.dk>
2020-10-23 13:22:18 +0200

    Basic setup needs to be edited to fit endpoints

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 import "./style.css"
 import "bootstrap/dist/css/bootstrap.css"
+import personFacade from "./personFacade";
 
 document.getElementById("all-content").style.display = "block"
 
 
 function hideAllShowOne(idToShow) {
   document.getElementById("about_html").style = "display:none"
-  document.getElementById("ex1_html").style = "display:none"
-  document.getElementById("ex2_html").style = "display:none"
-  document.getElementById("ex3_html").style = "display:none"
+  document.getElementById("search_html").style = "display:none"
+  document.getElementById("manage_person_html").style = "display:none"
   document.getElementById(idToShow).style = "display:block"
+  if(idToShow==="about_html"){
+    document.getElementById("personTable").style= "display:none"
+  }
+  else{
+    document.getElementById("personTable").style= "display:block"    
+  }
 }
 
 function menuItemClicked(evt) {
   const id = evt.target.id;
   switch (id) {
-    case "ex1": hideAllShowOne("ex1_html"); break
-    case "ex2": hideAllShowOne("ex2_html"); break
-    case "ex3": hideAllShowOne("ex3_html"); break
+    case "search": hideAllShowOne("search_html"); break
+    case "manage_person": hideAllShowOne("manage_person_html"); break
     default: hideAllShowOne("about_html"); break
   }
   evt.preventDefault();
@@ -25,5 +30,35 @@ function menuItemClicked(evt) {
 document.getElementById("menu").onclick = menuItemClicked;
 hideAllShowOne("about_html");
 
+function renderAllPersons(){
+
+  personFacade.getAllPersons().then((persons) => {
+    const personRows = persons.map(
+      (person) => `
+      <tr>
+        <td>
+        ${person.id}
+        </td>    
+        <td>
+        ${person.age}
+        </td>    
+        <td>
+        ${person.name}
+        </td>    
+        <td>
+        ${person.gender}
+        </td> 
+        <td>
+        ${person.email}
+        </td>  
+      </tr>
+      `
+    );
+    const personRowsAsString = personRows.join("");
+    //console.log(users);//To check if we get any data
+    document.getElementById("allPersonRows").innerHTML = personRowsAsString;
+  });
+}
+renderAllPersons();
 
 
---
+++ src/personFacade.js
@@ -0,0 +1,52 @@
+const URL = "http://localhost:3333/api/users/";
+
+function handleHttpErrors(res){
+    if(!res.ok){
+      return Promise.reject({status: res.status, fullError: res.json() })
+    }
+    return res.json();
+   }
+   
+
+  function makeOptions(method, body) {
+    var opts =  {
+      method: method,
+      headers: {
+        "Content-type": "application/json",
+        "Accept": "application/json"
+      }
+    }
+    if(body){
+      opts.body = JSON.stringify(body);
+    }
+    return opts;
+   }
+
+   function getAllPersons(){
+    return fetch(URL + "all") //Returns promise
+    .then(handleHttpErrors);
+   }
+   function addPerson(person){
+     let options = makeOptions("POST",person)
+    return fetch(URL,options) //Returns promise
+    .then(handleHttpErrors);
+   }
+   function editPerson(person){
+    let options = makeOptions("PUT", person);
+    // console.log(options);
+    return fetch(URL+id, options)
+    .then(handleHttpErrors);
+  }
+  function deletePerson(id) {
+    let options = makeOptions("DELETE");
+    return fetch(URL + id, options)
+    .then(handleHttpErrors);
+  }
+   const personFacade = {
+    //Object with the five functions in it
+    getAllPersons,
+    addPerson,
+    editPerson,
+    deletePerson
+  };
+   export default personFacade;
